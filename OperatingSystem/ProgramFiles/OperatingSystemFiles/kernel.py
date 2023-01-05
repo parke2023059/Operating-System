a27de439e7eba542f8b429c4b2bc9a7a67a80f30
Parker Fink <parke2023059@live.ytech.edu>
2023-01-05 10:31:49 -0500

    basic setup started

---
+++ OperatingSystem/ProgramFiles/OperatingSystemFiles/kernel.py
@@ -0,0 +1,21 @@
+import os
+
+
+
+
+class System():
+    
+    
+    #make, remove, rename, move folder
+    #make, remove, rename, move, file
+    
+    
+    
+    
+    
+    
+    #change directory
+    #list directory
+    
+    
+    
---
+++ OperatingSystem/ProgramFiles/OperatingSystemFiles/terminal.py
@@ -0,0 +1,13 @@
+from cmd import Cmd
+
+
+
+class Terminal(Cmd):
+    
+    def do_temp(self, blank):
+        print("Hello There")
+        
+        
+        
+    
+Terminal().cmdloop()

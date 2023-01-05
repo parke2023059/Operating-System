347dc1c21782e30a79e2f23a34ad5f8b9ae490db
Parker Fink <parke2023059@live.ytech.edu>
2023-01-05 10:42:19 -0500

    added ability to make and remove a folder with the directory

--- OperatingSystem/ProgramFiles/OperatingSystemFiles/kernel.py
+++ OperatingSystem/ProgramFiles/OperatingSystemFiles/kernel.py
@@ -1,5 +1,5 @@
 import os
-
+import shutil
 
 
 
@@ -9,13 +9,25 @@ class System():
     #make, remove, rename, move folder
     #make, remove, rename, move, file
     
+    #make folder
+    def makeFolder(path):
+        os.mkdir(path)
+        print(path + " Was Created ")
     
-    
-    
-    
+    def removeFolder(path):
+        shutil.rmtree(path)
+        print(path + " Was Removed ")
+
+    def renameFolder():
+        print("Currently Not Functional")    
+        
+
+        
+    def moveFolder():
+        print("Currently Not Functional")    
     
     #change directory
     #list directory
-    
-    
+    #clear 
+    #exit
     

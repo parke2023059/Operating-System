cca1a6c2c15c56124b084f80634a90f377d4e5a6
Parker Fink <parke2023059@live.ytech.edu>
2023-01-06 14:44:21 -0500

    as of now all errors

--- OperatingSystem/ProgramFiles/OS_FILES/terminal.py
+++ OperatingSystem/ProgramFiles/OS_FILES/terminal.py
@@ -5,15 +5,20 @@ from cmd import Cmd
 
 class Terminal(Cmd):
     
+    
     do_mkdir(self, path):
         os.mkdir(path)
         
     do_rmdir(self, path):
         shutil.rmtree(path)
     
+    
+    
     do_nf(self, path):
         os.system("echo > " + path)
 
+    do_rf(self, path):
+        os.rmdir(path)
 
     
     
